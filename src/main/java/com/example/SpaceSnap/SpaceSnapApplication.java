dcec9589139ae8edc4208d1a19675809e5fad726
Sophie <114300675+sophie4075@users.noreply.github.com>
2024-10-10 13:26:11 +0200

    Initial commit

---
+++ src/main/java/com/example/SpaceSnap/SpaceSnapApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpaceSnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceSnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpaceSnap/SpaceSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpaceSnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
